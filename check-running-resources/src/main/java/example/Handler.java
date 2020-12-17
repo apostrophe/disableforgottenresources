fb024874c78f67930ba64a826367ae662574f638
Ben Schilke <bschilke.biz@gmail.com>
2020-12-16 22:39:16 -0500

    stopping any running ec2 instances when invoked

--- check-running-resources/src/main/java/example/Handler.java
+++ check-running-resources/src/main/java/example/Handler.java
@@ -1,5 +1,13 @@
 package example;
 
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -20,46 +28,106 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.StringBuilder;
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 // Handler value: example.Handler
 public class Handler implements RequestHandler<SQSEvent, String>{
-  private static final Logger logger = LoggerFactory.getLogger(Handler.class);
-  private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-  private static final LambdaAsyncClient lambdaClient = LambdaAsyncClient.create();
-  public Handler(){
-    CompletableFuture<GetAccountSettingsResponse> accountSettings = lambdaClient.getAccountSettings(GetAccountSettingsRequest.builder().build());
-    try {
-      GetAccountSettingsResponse settings = accountSettings.get();
-    } catch(Exception e) {
-      e.getStackTrace();
-    }
-  }
-  @Override
-  public String handleRequest(SQSEvent event, Context context)
-  {
-    String response = new String();
-    // call Lambda API
-    logger.info("Getting account settings");
-    CompletableFuture<GetAccountSettingsResponse> accountSettings = 
-        lambdaClient.getAccountSettings(GetAccountSettingsRequest.builder().build());
-    // log execution details
-    logger.info("ENVIRONMENT VARIABLES: {}", gson.toJson(System.getenv()));
-    logger.info("CONTEXT: {}", gson.toJson(context));
-    logger.info("EVENT: {}", gson.toJson(event));
-    // process event
-    for(SQSMessage msg : event.getRecords()){
-      logger.info("~"+msg.getBody());
-    }
-    // process Lambda API response
-    try {
-      GetAccountSettingsResponse settings = accountSettings.get();
-      response = gson.toJson(settings.accountUsage());
-      logger.info("Account usage: {}", response);
-    } catch(Exception e) {
-      e.getStackTrace();
-    }
-    return response;
-  }
+	private static final Logger logger = LoggerFactory.getLogger(Handler.class);
+	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	private static final LambdaAsyncClient lambdaClient = LambdaAsyncClient.create();
+	public Handler(){
+		CompletableFuture<GetAccountSettingsResponse> accountSettings = lambdaClient.getAccountSettings(GetAccountSettingsRequest.builder().build());
+		try {
+			GetAccountSettingsResponse settings = accountSettings.get();
+		} catch(Exception e) {
+			e.getStackTrace();
+		}
+	}
+	@Override
+	public String handleRequest(SQSEvent event, Context context)
+	{
+
+		describeEc2Instances(context);
+
+
+		String response = new String();
+		// call Lambda API
+//		logger.info("Getting account settings");
+//		CompletableFuture<GetAccountSettingsResponse> accountSettings = 
+//				lambdaClient.getAccountSettings(GetAccountSettingsRequest.builder().build());
+//		// log execution details
+//		logger.info("ENVIRONMENT VARIABLES: {}", gson.toJson(System.getenv()));
+//		logger.info("CONTEXT: {}", gson.toJson(context));
+//		logger.info("EVENT: {}", gson.toJson(event));
+//		// process event
+//		for(SQSMessage msg : event.getRecords()){
+//			logger.info("~"+msg.getBody());
+//		}
+//		// process Lambda API response
+//		try {
+//			GetAccountSettingsResponse settings = accountSettings.get();
+//			response = gson.toJson(settings.accountUsage());
+//			logger.info("Account usage: {}", response);
+//		} catch(Exception e) {
+//			e.getStackTrace();
+//		}
+		return response;
+	}
+	
+	private void describeEc2Instances(Context context) {
+    	final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+    	boolean done = false;
+    	
+    	DescribeInstancesRequest request = new DescribeInstancesRequest();
+		DescribeInstancesResult response = ec2.describeInstances(request);
+		
+		//if (response.getNextToken() == null)
+		//	context.getLogger().log("No instances running!");
+		
+		out("--- RESOURCE START ---", context);
+		
+    	while (!done) {
+
+    		for(Reservation reservation : response.getReservations()) {
+    			for(Instance instance : reservation.getInstances()) {
+    				context.getLogger().log(String.format(
+    						"Instance %s, " + 
+    						"AMI: %s, " + 
+    						"type: %s, " + 
+    						"state: %s, ",
+    						instance.getInstanceId(),
+    						instance.getImageId(),
+    						instance.getInstanceType(),
+    						instance.getState()
+    						));
+    				
+//    				context.getLogger().log("getting tags...");
+    				List<Tag> tags = instance.getTags();
+    				for (Tag t : tags) {
+    					context.getLogger().log("instance.getState().getName()"+instance.getState().getName());
+    					if (t.getKey().contains("disable-candidate") && instance.getState().getName().equalsIgnoreCase("running")) {
+    						StopInstancesRequest stopRequest = new StopInstancesRequest().withInstanceIds(instance.getInstanceId());
+    						ec2.stopInstances(stopRequest);
+    						
+    						context.getLogger().log("*** request made to STOP instance " + instance.getInstanceId() + " ***");
+    						// add SNS notification
+    					}
+    				}
+    			}
+    		}
+    		
+    		request.setNextToken(response.getNextToken());
+    		if (response.getNextToken() == null) done = true;
+    	}
+    	
+		out("--- RESOURCE END ---", context);
+		
+	}
+	
+	private void out(String inString, Context context) {
+		System.out.println(inString);
+		context.getLogger().log(inString);
+	}
 }
